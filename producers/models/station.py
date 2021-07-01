0dfa7e0017a4b9df8466cc5e0611339a4ba3475d
Wittline <8701464+Wittline@users.noreply.github.com>
2021-07-01 17:27:21 -0500

    Update station.py

--- producers/models/station.py
+++ producers/models/station.py
@@ -15,6 +15,7 @@ class Station(Producer):
     """Defines a single station"""
 
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_key.json")
+    key_value = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
 
     #
     # TODO: Define this value schema in `schemas/station_value.json, then uncomment the below

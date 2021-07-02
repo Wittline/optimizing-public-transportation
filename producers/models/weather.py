476e574ca9e81c83be9f43dbb98a7f3aa5df384e
Wittline <8701464+Wittline@users.noreply.github.com>
2021-07-02 12:36:01 -0500

    Updating

--- producers/models/weather.py
+++ producers/models/weather.py
@@ -17,9 +17,7 @@ logger = logging.getLogger(__name__)
 class Weather(Producer):
     """Defines a simulated weather model"""
 
-    status = IntEnum(
-        "status", "sunny partly_cloudy cloudy windy precipitation", start=0
-    )
+    status = IntEnum("status", "sunny partly_cloudy cloudy windy precipitation", start=0)
 
     rest_proxy_url = "http://localhost:8082"
 
@@ -30,18 +28,15 @@ class Weather(Producer):
     summer_months = set((6, 7, 8))
 
     def __init__(self, month):
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
+
         super().__init__(
-            "weather", # TODO: Come up with a better topic name
+            "weather",
             key_schema=Weather.key_schema,
             value_schema=Weather.value_schema,
+            num_partitions=2,
+            num_replicas=2,
         )
-
+        
         self.status = Weather.status.sunny
         self.temp = 70.0
         if month in Weather.winter_months:
@@ -53,9 +48,6 @@ class Weather(Producer):
             with open(f"{Path(__file__).parents[0]}/schemas/weather_key.json") as f:
                 Weather.key_schema = json.load(f)
 
-        #
-        # TODO: Define this value schema in `schemas/weather_value.json
-        #
         if Weather.value_schema is None:
             with open(f"{Path(__file__).parents[0]}/schemas/weather_value.json") as f:
                 Weather.value_schema = json.load(f)
@@ -67,43 +59,27 @@ class Weather(Producer):
             mode = -1.0
         elif month in Weather.summer_months:
             mode = 1.0
+        
         self.temp += min(max(-20.0, random.triangular(-10.0, 10.0, mode)), 100.0)
         self.status = random.choice(list(Weather.status))
 
     def run(self, month):
         self._set_weather(month)
-
-        #
-        #
-        # TODO: Complete the function by posting a weather event to REST Proxy. Make sure to
-        # specify the Avro schemas and verify that you are using the correct Content-Type header.
-        #
-        #
-        logger.info("weather kafka proxy integration incomplete - skipping")
-        #resp = requests.post(
-        #    #
-        #    #
-        #    # TODO: What URL should be POSTed to?
-        #    #
-        #    #
-        #    f"{Weather.rest_proxy_url}/TODO",
-        #    #
-        #    #
-        #    # TODO: What Headers need to bet set?
-        #    #
-        #    #
-        #    headers={"Content-Type": "TODO"},
-        #    data=json.dumps(
-        #        {
-        #            #
-        #            #
-        #            # TODO: Provide key schema, value schema, and records
-        #            #
-        #            #
-        #        }
-        #    ),
-        #)
-        #resp.raise_for_status()
+        resp = requests.post(
+            f"{Weather.rest_proxy_url}/topics/{self.topic_name}",
+            headers={"Content-Type": "application/vnd.kafka.avro.v2+json"},
+            data=json.dumps(
+                {
+                    "key_schema": json.dumps(Weather.key_schema),
+                    "value_schema": json.dumps(Weather.value_schema),
+                    "records": [{
+                        "value": {"temperature": int(self.temp),"status": self.status.name},
+                        "key": {"timestamp": self.time_millis()}
+                    }]
+                }
+            ),
+        )             
+        resp.raise_for_status()
 
         logger.debug(
             "sent weather data to kafka, temp: %s, status: %s",

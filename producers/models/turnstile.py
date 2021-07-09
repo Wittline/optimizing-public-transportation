a0a2853776748e9c31859d691149187b42418e00
Wittline <8701464+Wittline@users.noreply.github.com>
2021-07-08 23:06:47 -0500

    Update turnstile.py

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -7,9 +7,7 @@ from confluent_kafka import avro
 from models.producer import Producer
 from models.turnstile_hardware import TurnstileHardware
 
-
 logger = logging.getLogger(__name__)
-
 
 class Turnstile(Producer):
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
@@ -20,10 +18,10 @@ class Turnstile(Producer):
         station_name = (station.name.lower().replace("/", "_and_").replace(" ", "_").replace("-", "_").replace("'", ""))
 
         super().__init__(
-            topic_name = 'topic_turnstile', 
+            topic_name = 'topic_turnstile',
             key_schema=Turnstile.key_schema,
             num_partitions=2,
-            num_replicas=2,
+            num_replicas=2
         )
 
         self.station = station
@@ -36,9 +34,10 @@ class Turnstile(Producer):
             self.producer.produce(
                 topic= self.topic_name,
                 key_schema={"timestamp": int(self.time_millis())},
-                value_schema={"station_id": self.station.station_id,
-                              "station_name": self.station.name,
-                              "line": self.station.color.name
+                value_schema={                                                            
+                    "station_id": self.station.station_id,
+                    "station_name": self.station.name,
+                    "line": self.station.color.name
                 }
             )
 
